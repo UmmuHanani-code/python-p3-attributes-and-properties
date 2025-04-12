7438ac379cff2ac5aa1f7125756a0e417fce6f64
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-04-13 01:21:31 +0300

    commit attributes and properties assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +10,38 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Golden", breed="Beagle"):
+        self._name = None
+        self._breed = None
+
+        valid_name = isinstance(name, str) and 1 <= len(name) <= 25
+        valid_breed = breed in APPROVED_BREEDS
+
+        if not valid_name:
+            print("Name must be string between 1 and 25 characters.")
+        if not valid_breed:
+            print("Breed must be in list of approved breeds.")
+
+        if valid_name and valid_breed:
+            self.name = name
+            self.breed = breed
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        self._name = name.title()
+        print(f"Setting name to {self._name}")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        print("Retrieving breed.")
+        return self._breed
+
+    def set_breed(self, breed):
+        self._breed = breed
+        print(f"Setting breed to {self._breed}")
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,38 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Admin"):
+        self._name = None
+        self._job = None
+
+        valid_name = isinstance(name, str) and 1 <= len(name) <= 25
+        valid_job = job in APPROVED_JOBS
+
+        if not valid_name:
+            print("Name must be string between 1 and 25 characters.")
+        if not valid_job:
+            print("Job must be in list of approved jobs.")
+
+        if valid_name and valid_job:
+            self.name = name
+            self.job = job
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        self._name = name.title()
+        print(f"Setting name to {self._name}")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        print("Retrieving job.")
+        return self._job
+
+    def set_job(self, job):
+        self._job = job
+        print(f"Setting job to {self._job}")
+
+    job = property(get_job, set_job)
